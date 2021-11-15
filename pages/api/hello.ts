a6394ff967c4514afc3669ccd3c5c1648b291a61
surazsunny965 <suraj.sunny.965@gmail.com>
2021-11-15 23:45:08 +0530

    second commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '.prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
 
-export default function handler(
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const prisma = new PrismaClient()
+  
+  let response = {}
+  if(req.method==="GET"){
+    response = await prisma.neerajfans.findMany({})
+  }
+  if (req.method==="POST"){
+    response = await prisma.neerajfans.create({data:{name:req.body.name}})
+  }
+  if (req.method==="DELETE"){
+    var userid = req.query.id
+     var lol:string = ""
+  if (userid.length>0){
+    lol = userid[0]
+  } 
+    response = await prisma.neerajfans.delete({where:{id:parseInt(lol)}})
+  }
+  res.status(200).json({ response })
 }
